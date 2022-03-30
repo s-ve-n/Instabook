e1634cc4205920cdd150e862996d17e09de6484d
plus <sven.radelof@gmail.com>
2022-03-30 14:46:45 +0000

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ImageCardComponent } from './image-card/image-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ImageCardComponent
   ],
   imports: [
     BrowserModule,
