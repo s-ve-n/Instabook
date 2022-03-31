35412f040b0d9776881b108f73ba021735f8e021
plus <sven.radelof@gmail.com>
2022-03-31 21:11:56 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   postTexts = [
     'This is a photo of a bay.',
-    'This is a photo of a beach.',
-    'This is a photo of a mountain.',
-    'This is a photo of a lake.',
+    'This is a photo of a meadow.',
+    'This is a photo of leaves.',
   ];
 
   postImg = [
     'https://ik.imagekit.io/2/photos/img/01.jpg',
     'https://ik.imagekit.io/2/photos/img/02.jpg',
-    'https://ik.imagekit.io/2/photos/img/03.jpg',
-    'https://ik.imagekit.io/2/photos/img/04.jpg',
+    'https://ik.imagekit.io/2/photos/img/07.jpg',
   ];
 
   buttonClicked() {
     alert('This is a test!');
   }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ImageCardComponent } from './image-card/image-card.component';
+import { ProposalsComponent } from './proposals/proposals.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ImageCardComponent
+    ImageCardComponent,
+    ProposalsComponent
   ],
   imports: [
     BrowserModule,
