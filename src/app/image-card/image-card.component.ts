2049eef1b542839099a406a496e7377e3a06974a
plus <sven.radelof@gmail.com>
2022-03-31 12:43:28 +0000

    update

--- src/app/image-card/image-card.component.ts
+++ src/app/image-card/image-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-card.component.scss']
 })
 export class ImageCardComponent implements OnInit {
+  @Input() text: string;
 
   constructor() { }
 
