946968efa631f6a2f379a8c1463fa5acfca68863
plus <sven.radelof@gmail.com>
2022-04-01 14:52:42 +0000

    update

--- src/app/profile-row/profile-row.component.ts
+++ src/app/profile-row/profile-row.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-row',
   templateUrl: './profile-row.component.html',
-  styleUrls: ['./profile-row.component.scss']
+  styleUrls: ['./profile-row.component.scss'],
 })
 export class ProfileRowComponent implements OnInit {
+  @Input() name: string = 'Bay';
+  @Input() img: string = 'https://ik.imagekit.io/2/photos/img/01.jpg';
+  @Input() description: string = 'This is a photo of a bay.';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/proposals/proposals.component.ts
+++ src/app/proposals/proposals.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-proposals',
   templateUrl: './proposals.component.html',
-  styleUrls: ['./proposals.component.scss']
+  styleUrls: ['./proposals.component.scss'],
 })
 export class ProposalsComponent implements OnInit {
+  names = ['Friend1', 'Friend2', 'Friend3'];
+  texts = [
+    'This is a photo of a bay.',
+    'This is a photo of a meadow.',
+    'This is a photo of leaves.',
+  ];
+  images = [
+    'https://ik.imagekit.io/2/photos/img/01.jpg',
+    'https://ik.imagekit.io/2/photos/img/02.jpg',
+    'https://ik.imagekit.io/2/photos/img/07.jpg',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
